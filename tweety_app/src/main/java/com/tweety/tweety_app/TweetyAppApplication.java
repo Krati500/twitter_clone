158c90b4f385cf178ec2d43ca53cc51b85f0064f
Krati Bhandari <bhandari.krati98@gmail.com>
2021-11-10 17:25:36 +0530

    initial project

---
+++ tweety_app/src/main/java/com/tweety/tweety_app/TweetyAppApplication.java
@@ -0,0 +1,13 @@
+package com.tweety.tweety_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetyAppApplication.class, args);
+	}
+
+}
---
+++ tweety_app/src/test/java/com/tweety/tweety_app/TweetyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweety.tweety_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
