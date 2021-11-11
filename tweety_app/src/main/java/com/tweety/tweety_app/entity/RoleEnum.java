49861e3be2a342f1224fcab208568cae8bcec52d
Krati Bhandari <bhandari.krati98@gmail.com>
2021-11-11 14:04:32 +0530

    Role enum added

---
+++ tweety_app/src/main/java/com/tweety/tweety_app/entity/RoleEnum.java
@@ -0,0 +1,6 @@
+package com.tweety.tweety_app.entity;
+
+public enum RoleEnum {
+	ADMIN,USER
+
+}
--- tweety_app/src/main/java/com/tweety/tweety_app/entity/TweetEnitity.java
+++ tweety_app/src/main/java/com/tweety/tweety_app/entity/TweetEnitity.java
@@ -1,5 +1,29 @@
 package com.tweety.tweety_app.entity;
 
-public class TweetEnitity {
+import java.security.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.apache.catalina.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
+
+@Entity
+public class TweetEnitity {
+	@Id
+	private Integer id;
+	
+	@CreationTimestamp
+	private Timestamp postTime;
+	
+	@ManyToOne
+	private User tweetUser;
+	
+	@NotNull
+	private String content;
+	
+	
+  
 }
